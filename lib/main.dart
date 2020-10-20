c46af370586429eb0fbcc009db6c4dc072c27d8e
jfinkley <43913359+jfinkley@users.noreply.github.com>
2020-10-19 22:47:02 -0500

    Adds missing Android XML

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "package:tigermap/screens/home_screen.dart";
+import "package:skegee_map/screens/home_screen.dart";
 
 void main() {
   runApp(MyApp());
