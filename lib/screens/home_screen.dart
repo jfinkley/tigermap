11600c3032ff8c206c5597ece091ca2c2bcb0f65
jfinkley <43913359+jfinkley@users.noreply.github.com>
2020-10-24 11:34:37 -0500

    Adds Google Map widget to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,59 +1,66 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:skegee_map/screens/search.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xffE3B821),
-      body: SizedBox.expand(
-        child: DraggableScrollableSheet(
-          initialChildSize: 0.5,
-          maxChildSize: 0.8,
-          minChildSize: 0.2,
-          builder: (BuildContext context, ScrollController scrollController) {
-            return Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20),
-                  color: Colors.blue[100],
-                ),
-                child: Scaffold(
-                    appBar: AppBar(
-                  backgroundColor: Colors.red,
-                  title: Center(
-                    child: Text('Navigate Tuskegee'),
-                  ),
-                  leading: Icon(Icons.push_pin_rounded),
-                  actions: [
-                    IconButton(
-                        icon: Icon(Icons.search_outlined), onPressed: () {
-                          showSearch(context: context, delegate: BuildingSearch());
-                    })
-                  ],
-                )
-                    // TextField(
-                    //   enableSuggestions: true,
-                    //   autofillHints: ['Rockefeller', 'LTRIMM'],
-                    //   decoration:InputDecoration(
-                    //     border: OutlineInputBorder(),
-                    //     labelText:'Search Campus',
-                    //     suffixIcon: Icon(
-                    //       Icons.search
-                    //     )
-                    //   ),
-                    // ),
-                    )
-                //ListView.builder(
-                //   controller: scrollController,
-                //   itemCount: 25,
-                //   itemBuilder: (BuildContext context, int index) {
-                //     return ListTile(title: Text('Item $index'));
-                //   },
-                // ),
-                );
-          },
-        ),
-      ),
+      body: Search(),
+      // body: Column(
+      //   children: [
+      //     Search(),
+      //     SizedBox.expand(
+      //       child: DraggableScrollableSheet(
+      //         initialChildSize: 0.4,
+      //         maxChildSize: 0.6,
+      //         minChildSize: 0.2,
+      //         builder: (BuildContext context, ScrollController scrollController) {
+      //           return Container(
+      //               decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.circular(20),
+      //                 color: Colors.blue[100],
+      //               ),
+      //               child: Scaffold(
+      //                   appBar: AppBar(
+      //                 backgroundColor: Colors.red,
+      //                 title: Center(
+      //                   child: Text('Navigate Tuskegee'),
+      //                 ),
+      //                 leading: Icon(Icons.push_pin_rounded),
+      //                 actions: [
+      //                   IconButton(
+      //                       icon: Icon(Icons.search_outlined), onPressed: () {
+      //                         showSearch(context: context, delegate: BuildingSearch());
+      //                   })
+      //                 ],
+      //               )
+      //                   // TextField(
+      //                   //   enableSuggestions: true,
+      //                   //   autofillHints: ['Rockefeller', 'LTRIMM'],
+      //                   //   decoration:InputDecoration(
+      //                   //     border: OutlineInputBorder(),
+      //                   //     labelText:'Search Campus',
+      //                   //     suffixIcon: Icon(
+      //                   //       Icons.search
+      //                   //     )
+      //                   //   ),
+      //                   // ),
+      //                   )
+      //               //ListView.builder(
+      //               //   controller: scrollController,
+      //               //   itemCount: 25,
+      //               //   itemBuilder: (BuildContext context, int index) {
+      //               //     return ListTile(title: Text('Item $index'));
+      //               //   },
+      //               // ),
+      //               );
+      //         },
+      //       ),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 class Search extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height/3,
+            width: MediaQuery.of(context).size.width,
+            child: GoogleMap(
+              initialCameraPosition:
+              CameraPosition(
+                  target:
+                  LatLng(32.427183, -85.702267)),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
