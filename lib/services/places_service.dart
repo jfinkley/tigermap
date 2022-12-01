b8a4446ef84d2b1822492f079e149aa78ec2bc69
jfinkley <jfinkley8680@tuskegee.edu>
2022-12-01 11:57:42 -0600

    removes key from versioned file

--- lib/services/places_service.dart
+++ lib/services/places_service.dart
@@ -1,6 +1,6 @@
 class PlacesService{
   // Google Maps Key Goes Here
-  final key = 'AIzaSyA0X-xSLh5klyUM9aToq5JPqZbOH8ssjgs';
+  final key = '';
 
   int getPlace(){
     print('Get Place Called');
